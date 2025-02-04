aa709ac66714e8d584f487bbd6ddc5bbbe819b23
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2025-02-04 21:13:16 +0530

    everything working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {EventData} from './model/event-data';
+import {EventService} from './service/event.service';
+import {EventComponent} from './component/event/event.component';
+import {HeaderComponent} from './component/header/header.component';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
+import {AddEventComponent} from './component/add-event/add-event.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EventComponent, HeaderComponent, MatGridList, MatGridTile, AddEventComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'event-management-ui-for-lecture';
+  title = 'Event Management';
+  events: EventData[] = [];
+  eventService = inject(EventService);
+
+  constructor() {
+    this.eventService.getAllEvents()
+      .then((events) => {
+        this.events = events;
+      })
+  }
+  deleteEvent($event: EventData) {
+    this.events.splice(this.events.indexOf($event), 1);
+  }
 }
